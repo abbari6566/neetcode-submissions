9b804b6315f667dbf58f95aa1e4a678eeff3d8d5
Abrar F. Bari <barifaiazabrar@gmail.com>
2026-09-09 20:05:26 -0500

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0],nums[1])
+
+        #first case -> consider house 1 (nums[0]) and not nums[n-1]
+        prev, curr = nums[0], max(nums[0], nums[1])
+        for i in range(2,n-1):
+            prev, curr = curr, max(prev + nums[i], curr)
+        
+        prev_, curr_ = nums[1], max(nums[1], nums[2])
+        for i in range(3,n):
+            prev_, curr_ = curr_ , max(prev_ + nums[i], curr_)
+        
+        return max(curr, curr_)
+
+
+        
